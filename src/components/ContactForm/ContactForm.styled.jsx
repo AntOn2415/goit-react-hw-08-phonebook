b3a9102bf011aaa0840de4dafcad1fba476aee47
AntOnAntOff <povanton@gmail.com>
2023-08-13 21:37:46 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 import PhoneInput from 'react-phone-number-input';
-import {VisibilityHidden, FormCommon, InputFormCommon, LabelCommon, IconWrapperCommon, InputCommon, BtnCommon } from '../componentStyles';
+import {
+  VisibilityHidden,
+  FormCommon,
+  InputFormCommon,
+  LabelCommon,
+  IconWrapperCommon,
+  InputCommon,
+  BtnCommon,
+} from '../componentStyles';
 
 export const TitleHiddenH2 = styled.h2`
   ${VisibilityHidden};
-`
+`;
 
 export const FormContainer = styled.div`
   position: sticky;
@@ -47,7 +55,7 @@ export const InputForm = styled.input`
   ${InputCommon};
   border: ${({ theme }) => theme.borders.border};
   font-size: ${({ theme }) => theme.fontSizes.medium};
-  border-radius:  ${({ theme }) => theme.borders.borderRadius};
+  border-radius: ${({ theme }) => theme.borders.borderRadius};
   color: ${({ theme }) => theme.colors.text};
   background-color: ${({ theme }) => theme.colors.backgroundSecondary};
   ${InputFormCommon};
@@ -67,47 +75,45 @@ export const InputForm = styled.input`
 
   ::placeholder {
     color: ${({ theme }) => theme.colors.placeholder};
-  };
-  
+  }
 `;
 
 export const StyledPhoneInput = styled(PhoneInput)`
-
   input {
     ${InputCommon};
     display: flex;
     align-items: center;
     width: calc(100% - 46px);
-  border: ${({ theme }) => theme.borders.border};
-  font-size: ${({ theme }) => theme.fontSizes.medium};
-  border-radius:  ${({ theme }) => theme.borders.borderRadius};
-  color: ${({ theme }) => theme.colors.text};
-  background-color: ${({ theme }) => theme.colors.backgroundSecondary};
-  ${InputFormCommon};
+    border: ${({ theme }) => theme.borders.border};
+    font-size: ${({ theme }) => theme.fontSizes.medium};
+    border-radius: ${({ theme }) => theme.borders.borderRadius};
+    color: ${({ theme }) => theme.colors.text};
+    background-color: ${({ theme }) => theme.colors.backgroundSecondary};
+    ${InputFormCommon};
 
-  :hover,
-  :focus {
-    border-color: ${({ theme }) => theme.borders.borderColorHover};
-  }
+    :hover,
+    :focus {
+      border-color: ${({ theme }) => theme.borders.borderColorHover};
+    }
 
-  :not(:placeholder-shown):required:valid {
-    border-color: ${({ theme }) => theme.borders.borderColorValid};
-  }
+    :not(:placeholder-shown):required:valid {
+      border-color: ${({ theme }) => theme.borders.borderColorValid};
+    }
 
-  :not(:placeholder-shown):required:invalid {
-    border-color: ${({ theme }) => theme.borders.borderColorInvalid};
-  }
+    :not(:placeholder-shown):required:invalid {
+      border-color: ${({ theme }) => theme.borders.borderColorInvalid};
+    }
 
-  ::placeholder {
-    color: ${({ theme }) => theme.colors.placeholder};
-  };
+    ::placeholder {
+      color: ${({ theme }) => theme.colors.placeholder};
+    }
   }
 
   .PhoneInputCountryIcon {
     position: absolute;
-    top: 50%;
+    top: 48%;
     left: 6px;
-    height: 16px;
+    height: 17px;
     width: 25px;
     border: ${({ theme }) => theme.borders.border};
   }
@@ -126,21 +132,25 @@ export const StyledPhoneInput = styled(PhoneInput)`
 
 export const BtnForm = styled.button`
   ${BtnCommon};
-font-size: ${({ theme }) => theme.fontSizes.medium};
+  font-size: ${({ theme }) => theme.fontSizes.medium};
   background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.white};
-  border-radius:  ${({ theme }) => theme.borders.borderRadius};
+  border-radius: ${({ theme }) => theme.borders.borderRadius};
   cursor: ${props => (props.isLoading ? 'not-allowed' : 'pointer')};
   box-shadow: ${({ theme }) => theme.shadows.default};
 
   :hover,
   :focus {
     box-shadow: ${({ theme }) => theme.shadows.hover};
-    background-color: ${props => (props.isLoading ? props.theme.colors.primary : props.theme.colors.secondary)};
+    background-color: ${props =>
+      props.isLoading
+        ? props.theme.colors.primary
+        : props.theme.colors.secondary};
   }
 
   :disabled {
     background-color: rgba(39, 174, 96, 0.6);
     box-shadow: ${({ theme }) => theme.shadows.default};
     cursor: not-allowed;
-  }`;
+  }
+`;
